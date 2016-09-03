0f61b952afc37e60259d5d127cf213dfdc038e35
shra012 <shravan_fisher@yahoo.ca>
2016-09-04 01:33:40 +0530

    Added my first set questions

---
+++ ZQuestionSet/src/com/shrvn/set1/PrintTheWordWithOddLetters.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.shrvn.set1;
+
+/**
+ * @author Shravan
+ *
+ */
+public class PrintTheWordWithOddLetters {
+
+	/**
+	 * Print the word with odd letters as 
+	 *  Input: 
+		PROGRAM
+		Output: 
+		 P     M
+		  R   A
+		   O R
+		    G
+		   O R
+		  R   A
+		 P     M 
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
