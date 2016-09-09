456623687771be22ad7fc1e34769523f9f44f334
Shravankumar Nagarajan <shravan_fisher@yahoo.ca>
2016-09-09 20:18:23 +0530

    Added Time Capsue code

--- Arrays/src/com/shrvn/matrix/TimeMatrix.java
+++ Arrays/src/com/shrvn/matrix/TimeMatrix.java
@@ -3,6 +3,8 @@
  */
 package com.shrvn.matrix;
 
+import java.util.Arrays;
+
 /**
  * @author 965922
  *
@@ -36,13 +38,50 @@ public class TimeMatrix {
 			          {9, 10, 11, 12},
 			          {13, 14, 15, 16}
 			        };
-		
+		int x = a[0].length;
+		int y = a.length;
 		//find all possible time capsules
-		getTimeCapsule();
+		getTimeCapsule(a,x,y);
+		
 	}
 	
-	public static void getTimeCapsule(){
-		
+	public static void getTimeCapsule(int a[][],int x,int y){
+		int j=0;
+		int[] suma = new int[x+y];
+		int sum=0;
+		int index=0;
+		for(int i =0;j<x;i++){
+			try{
+				System.out.println(a[j][i]+""+a[j][i+1]+""+a[j][i+2]+""+a[j+1][i+1]+""+a[j+2][i]+""+a[j+2][i+1]+""+a[j+2][i+2]);
+			sum = a[j][i]+a[j][i+1]+a[j][i+2]+a[j+1][i+1]+a[j+2][i]+a[j+2][i+1]+a[j+2][i+2];
+			suma[index]=sum;
+			index++;
+			}catch(ArrayIndexOutOfBoundsException e){
+				j++;
+				i=-1;
+				continue;
+			}
+		}
+		int i=0;
+		for(j =0;i<y;j++){
+			try{
+				System.out.println(a[j][i]+""+a[j+1][i]+""+a[j+2][i]+""+a[j+1][i+1]+""+a[j][i+2]+""+a[j+1][i+2]+""+a[j+2][i+2]);
+			sum = a[j][i]+a[j+1][i]+a[j+2][i]+a[j+1][i+1]+a[j][i+2]+a[j+1][i+2]+a[j+2][i+2];
+			suma[index]=sum;
+			index++;
+			}catch(ArrayIndexOutOfBoundsException e){
+				i++;
+				j=-1;
+				continue;
+			}
+		}
+		System.out.println(Arrays.toString(suma));
+		int temp=Integer.MIN_VALUE;
+		for(int l = 0;l<suma.length-1;l++){
+			if(temp<suma[l]){
+				temp=suma[l];
+			}
+		}
+		System.out.println("max of sum of integers in all possible time capsule "+temp);
 	}
-
 }
