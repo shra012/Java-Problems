04792614c1ee553c3c2f5aed8a1cda959a2443cb
Shravankumar Nagarajan <shravan_fisher@yahoo.ca>
2016-09-22 02:23:46 +0530

    completed working logic of quick sort

--- Arrays/src/com/shrvn/arrays/algo/QuickSort.java
+++ Arrays/src/com/shrvn/arrays/algo/QuickSort.java
@@ -17,28 +17,31 @@ public class QuickSort {
 		int end = a.length-1;
 		int start = 0;
 		//swap(a,0,7);
-		//printArray(a);
 		
-		//quickSort(a,start,end);
+		quickSort(a,start,end);
+		printArray(a);
 	}
 	
     public static void quickSort(int a[],int start,int end) {
     	if(start<end){
-    	
-    		
+    	int pIndex = partition(a,start,end);
+    	quickSort(a,start,pIndex-1);
+    	quickSort(a,pIndex+1,end);
     	}
 	}
     
-    public static void partition(int a[],int start,int end) {
+    public static int partition(int a[],int start,int end) {
     	int pivot = a[end];
-		int pIndex=a[start];
+		int pIndex=start;
 		//Swap method
 		for(int i=start;i<end;i++){
 			if(a[i]<=pivot){
-				swap(a,a[i],a[pIndex]);
-				pIndex = pIndex+1;
+				swap(a,i,pIndex);
+				pIndex++;
 			}
 		}
+		swap(a,pIndex,end);
+		return pIndex;
 	}
     
     public static void printArray(int a[]) {
@@ -52,9 +55,11 @@ public class QuickSort {
     
     public static void swap(int a[],int x, int y){
     	//if x = 10 y= 5
+    	  if(x!=y){
     	  a[x] = a[x] ^a[y];  // x now becomes 15 (1111)
     	  a[y] = a[x] ^ a[y];  // y becomes 10 (1010)
     	  a[x] = a[x] ^ a[y];  // x becomes 5 (0101)
+    	  }
     }
 
 }
