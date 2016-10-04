1dda060a84fa332127048809a2087f5103be3ef3
Shravankumar Nagarajan <shravan_fisher@yahoo.ca>
2016-10-04 19:38:28 +0530

    Added logic from geeksforgeeks

--- Arrays/src/com/shrvn/arrays/reverse/MergeAnArrayOfSizeNIntoAnotherArrayOfSizeMN.java
+++ Arrays/src/com/shrvn/arrays/reverse/MergeAnArrayOfSizeNIntoAnotherArrayOfSizeMN.java
@@ -20,6 +20,9 @@ public class MergeAnArrayOfSizeNIntoAnotherArrayOfSizeMN {
 	      int N[] = {5, 7, 9, 25};
 	      //merge.move(mPlusN,N);
 	      merge.moveToEnd(mPlusN);
+	      System.out.println(Arrays.toString(N));
+	      merge.merge(mPlusN, N);
+	      System.out.println(Arrays.toString(mPlusN));
 	}
 	
 	public void moveToEnd(int a[]) {
@@ -30,9 +33,30 @@ public class MergeAnArrayOfSizeNIntoAnotherArrayOfSizeMN {
 			if(a[i]!=-1){
 				a[j]=a[i];
 				j--;	
+			}if(i==-1){
+				break shravan;
 			}
 		}
 		System.out.println(Arrays.toString(a));
+	}
+	public static void merge(int a[],int b[]){
+		int al=a.length;
+		int bl=b.length;
+		int i = bl;
+		int j=0;
+		int k=0;
+		while (k<al){
+			if(i<al && a[i]<=b[j] || j==al){
+				a[k]=a[i];
+				k++;
+				i++;
+			}else{
+				a[k]=b[j];
+				k++;
+				j++;
+			}
+		}
+		
 	}
 	
 	//my method O(n2) time complexity
