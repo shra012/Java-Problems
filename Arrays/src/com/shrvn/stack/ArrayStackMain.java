354de0d92eb7ebf282f3c14c68b2c681d64cdddb
Shravankumar Nagarajan <shravan_fisher@yahoo.ca>
2016-10-07 00:24:46 +0530

    Added switch case for my stack

--- Arrays/src/com/shrvn/stack/ArrayStackMain.java
+++ Arrays/src/com/shrvn/stack/ArrayStackMain.java
@@ -3,6 +3,8 @@
  */
 package com.shrvn.stack;
 
+import java.util.Scanner;
+
 import com.shrvn.ds.stack.ArrayStack;
 import com.shrvn.ds.stack.Stack;
 
@@ -10,14 +12,39 @@ import com.shrvn.ds.stack.Stack;
  * @author shravan
  *
  */
-public class ArrayStackMain {
+public class ArrayStackMain<E> {
 
 	/**
 	 * Driver method to test my array stack
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
+		System.out.println("Welcome To Shravan ArrayStack");
+		Scanner sc = new Scanner(System.in);
+		String input=null;
+		ArrayStackMain<Integer> stack = new ArrayStackMain<>();
 		Stack<Integer> S = new ArrayStack<>();
+		do{
+			System.out.println("Please select a option");
+			System.out.println("1.push");
+			System.out.println("2.pop");
+			System.out.println("3.top");
+			System.out.println("4.size");
+			System.out.println("5.print Stack");
+			String swicth = sc.next();
+			stack.switchCase(swicth,S,sc);
+			System.out.println("Do you want to continue? (Y/N)");
+			input = sc.next();
+			if(!("Y".equals(input) ||  "N".equals(input))){
+				System.out.println("Sorry Wrong input.. Please enter Y or N");
+				input=sc.next();
+			}
+		}while("Y".equals(input));
+		System.out.println("Thank You :) ");
+		sc.close();
+
+		/*Stack<Integer> S = new ArrayStack<>();
 		S.push(5);
 		S.push(3);
 		System.out.println(S.size());
@@ -35,7 +62,40 @@ public class ArrayStackMain {
 		S.push(223);
 		System.out.println(S.pop());
 		ArrayStack<Integer> A=(ArrayStack<Integer>) S;
-		A.printStack(A.getData());
+		A.printStack(A.getData());*/
 	}
+	@SuppressWarnings("unchecked")
+	public void switchCase(String expression,Stack<E> s,Scanner sc){
 
+
+		switch(expression) {
+		case "1" :
+			Integer integer=null;
+				System.out.println("Enter the element to push");
+				integer=sc.nextInt();	
+			s.push((E) integer);
+			switchCase("5",s,sc);
+			break; // optional
+
+		case "2" :
+			s.pop();
+			switchCase("5",s,sc);
+			break; // optional
+		case "3" :
+			s.top();
+			switchCase("5",s,sc);
+			break; // optional
+		case "4" :
+			System.out.println("size of stack"+s.size());
+			break; // optional
+		case "5" :
+			ArrayStack<Integer> A=(ArrayStack<Integer>) s;
+			A.printStack(A.getData());
+			break; // optional
+
+			// You can have any number of case statements.
+		default : // Optional
+			System.out.println("Sorry Wrong Option ...");
+		}
+	}
 }
