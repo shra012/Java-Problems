2983f61e3f9120bd0864717475921632cfbd8423
shra012 <shravan_fisher@yahoo.ca>
2016-10-08 21:08:10 +0530

    Added Code for Ruler Print

--- Problems/src/com/shrvn/recursion/EnglishRuler.java
+++ Problems/src/com/shrvn/recursion/EnglishRuler.java
@@ -16,19 +16,42 @@ public class EnglishRuler {
 	public static void configure() {
 		BasicConfigurator.configure();
 	}
+
 	public static void main(String[] args) {
 		configure();
-		logger.debug("hello");
+		logger.debug("Inside main method");
+		drawRuler(2,4);
+		logger.debug("Leaving main method");
 	}
-	public static void drawRuler(int nInches,int majorLength) {
 
-	}
-	public static void drawLine(int tickLength) {
-		drawLine(tickLength,1);
-	} 
-	public static void drawLine(int tickLength, int lable) {
-		for (int i = 0; i < tickLength; i++) {
-
+	public static void drawRuler(int nInches, int majorLength) {
+		drawLine(majorLength, 0);
+		for (int j = 1; j <= nInches; j++) {
+			drawInterval(majorLength - 1);
+			drawLine(majorLength, j);
 		}
 	}
+
+	public static void drawInterval(int centralLength) {
+		if (centralLength >= 1) {
+			drawInterval(centralLength - 1);
+			drawLine(centralLength);
+			drawInterval(centralLength - 1);
+		}
+	}
+
+	public static void drawLine(int tickLength, int lable) {
+		for (int i = 0; i < tickLength; i++){
+			System.out.print("-");
+		}
+		if (lable >= 0) {
+			System.out.print(" " + lable);
+		}
+		System.out.println();
+	}
+
+	public static void drawLine(int tickLength) {
+		drawLine(tickLength, -1);
+	}
+
 }
