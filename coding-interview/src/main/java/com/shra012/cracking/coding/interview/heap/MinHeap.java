e4708fde071315858deb1d2f3f79cc3a884af1bb
shra012 <shravan_fisher@yahoo.ca>
2021-12-25 22:24:44 +0530

    Minimum Heap Implementation.

---
+++ coding-interview/src/main/java/com/shra012/cracking/coding/interview/heap/MinHeap.java
@@ -0,0 +1,61 @@
+package com.shra012.cracking.coding.interview.heap;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MinHeap<T extends Comparable<T>> extends Heap<T> {
+
+    protected MinHeap(Class<T> clazz) {
+        super(clazz);
+    }
+
+    protected MinHeap(Class<T> clazz, int size) {
+        super(clazz, size);
+    }
+
+    @Override
+    protected void siftDown(int index) {
+        log.info("Sifting down: {}", get(index));
+        int rightIndex = getRightChildIndex(index);
+        int leftIndex = getLeftChildIndex(index);
+        int smallerIndex = -1;
+        if (rightIndex != -1 && leftIndex != -1) {
+            T rightValue = get(rightIndex);
+            T leftValue = get(leftIndex);
+            smallerIndex = (leftValue.compareTo(rightValue) <= 0) ? leftIndex : rightIndex;
+        } else if (leftIndex != -1) {
+            smallerIndex = leftIndex;
+        } else {
+            smallerIndex = rightIndex;
+        }
+        if (smallerIndex == -1) {
+            log.info("Stop sifting down, found position");
+            return;
+        }
+        log.info("Smaller child: {}", get(smallerIndex));
+        if (get(smallerIndex).compareTo(get(index)) <= 0) {
+            swap(smallerIndex, index);
+            log.info("Swap : current - {} with actual {}", get(index), get(smallerIndex));
+            siftDown(smallerIndex);
+        }
+    }
+
+    @Override
+    protected void siftUp(int index) {
+        log.info("Sifting up: {}", get(index));
+        int parentIndex = getParentIndex(index);
+        if (parentIndex == -1) {
+            log.info("Stop sifting up, found position");
+            return;
+        }
+        log.info("Parent : {}", get(parentIndex));
+        T childValue = get(index);
+        T parentValue = get(parentIndex);
+        if (childValue.compareTo(parentValue) <= 0) {
+            swap(parentIndex, index);
+            log.info("Swap : current - {} with parent {}", get(index), get(parentIndex));
+            siftUp(parentIndex);
+        }
+
+    }
+}
---
+++ coding-interview/src/test/java/com/shra012/cracking/coding/interview/heap/MinHeapTests.java
@@ -0,0 +1,87 @@
+package com.shra012.cracking.coding.interview.heap;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MinHeapTests {
+    private final MinHeap<Integer> minHeap = new MinHeap<>(Integer.class);
+
+    @BeforeEach
+    void setUp() throws HeapFullException {
+        minHeap.add(9);
+        minHeap.add(19);
+        minHeap.add(29);
+        minHeap.add(39);
+        minHeap.add(49);
+        minHeap.add(59);
+        minHeap.add(69);
+        System.out.println(minHeap);
+    }
+
+    @Test
+    void shouldInsertValueIntoTheHeap() throws HeapFullException {
+        Assertions.assertEquals(7, minHeap.getSize());
+        minHeap.add(1);
+        Assertions.assertEquals(8, minHeap.getSize());
+    }
+
+    @Test
+    void shouldHaveTheSmallestValueAsThePriorityElement() throws HeapFullException, HeapEmptyException {
+        minHeap.add(5);
+        minHeap.add(200);
+        Assertions.assertEquals(5, minHeap.getHighestPriority());
+    }
+
+    @Test
+    void shouldThrowHeapFullExceptionWhenHeapIsFull() throws HeapFullException {
+        minHeap.add(100);
+        minHeap.add(200);
+        minHeap.add(79);
+        Assertions.assertThrows(HeapFullException.class, () -> minHeap.add(89));
+    }
+
+    @Test
+    void shouldThrowHeapEmptyExceptionWhenHeapIsFull() {
+        MinHeap<Integer> minHeap = new MinHeap<>(Integer.class, 1);
+        Assertions.assertThrows(HeapEmptyException.class, minHeap::getHighestPriority);
+    }
+
+    @Test
+    void shouldRemoveHighestPriorityElement() throws HeapEmptyException {
+        Assertions.assertEquals(9, minHeap.removeHighestPriority());
+        System.out.println(minHeap);
+    }
+
+    @Test
+    void shouldRemoveHighestPriorityElementAndSiftDown() throws HeapFullException, HeapEmptyException {
+        MinHeap<Integer> minHeap = new MinHeap<>(Integer.class);
+        minHeap.add(1);
+        minHeap.add(2);
+        minHeap.add(3);
+        Assertions.assertEquals(1, minHeap.removeHighestPriority());
+        Assertions.assertEquals(2, minHeap.removeHighestPriority());
+        Assertions.assertEquals(3, minHeap.removeHighestPriority());
+    }
+
+    @Test
+    void shouldReturnFullIfTheHeapIsFull() throws HeapEmptyException, HeapFullException {
+        minHeap.add(12);
+        minHeap.add(24);
+        minHeap.add(36);
+        Assertions.assertTrue(minHeap.isFull());
+        minHeap.removeHighestPriority();
+        Assertions.assertFalse(minHeap.isFull());
+    }
+
+    @Test
+    void shouldReturnNegativeOneIfInvalidIndexIsPassedToGetParentIndex() {
+        Assertions.assertEquals(-1, minHeap.getParentIndex(-2));
+    }
+
+    @Test
+    void shouldReturnNegativeOneIfInvalidIndexIsPassedToGetChildIndex() {
+        Assertions.assertEquals(-1, minHeap.getLeftChildIndex(-2));
+        Assertions.assertEquals(-1, minHeap.getRightChildIndex(-2));
+    }
+}
