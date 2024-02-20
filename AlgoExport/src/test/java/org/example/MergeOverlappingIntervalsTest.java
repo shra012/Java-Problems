7edf811457ba80ff94ce2c0ed508e68be78a8cf4
shra012 <shravan_fisher@yahoo.ca>
2024-02-20 10:42:37 +0530

    Added AlgoExpert

---
+++ AlgoExport/src/test/java/org/example/MergeOverlappingIntervalsTest.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class MergeOverlappingIntervalsTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public MergeOverlappingIntervalsTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( MergeOverlappingIntervalsTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
